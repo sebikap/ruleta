2a842a1249455785a3945912f009118af6722f89
sebikap <sebikap@hotmail.com>
2021-03-16 10:31:44 -0300

    Add files via upload

--- preguntas.js
+++ preguntas.js
@@ -1,6 +1,18 @@
+var options = [
+    { nombre: 'Psicoanalisis', texto: 'Psicoanálisis' },
+    { nombre: 'Cognitivo', texto: 'Cognitivo\nConductual' },
+    { nombre: 'Sistemico', texto: 'Sistémico' },
+    { nombre: 'Humanista', texto: 'Humanista\nExistencial' },
+    { nombre: 'Integrativo', texto: 'Integrativo' },
+    { nombre: 'Psicoeducacional', texto: 'Psicoeducacional' },
+    { nombre: 'Psicolaboral', texto: 'Psicolaboral' },
+    { nombre: 'Social', texto: 'Psicología\nSocial/Comunitaria' },
+    { nombre: 'Clinica', texto: 'Viñeta\nClínica' }
+];
+
 var preguntas = 
 [
-    {"categoria": "Psicoanalisis", "texto": "¿Cómo se llama la relación del paciente/analista?"},  
+    {"categoria": "Psicoanalisis", "texto": "¿Qué diferencia hay entre alianza terapéutica y transferencia?"},  
     {"categoria": "Cognitivo", "texto": "¿Cuáles son las características principales de este modelo teórico? Nombra alguna técnica que sepas o que te acuerdes"},
     {"categoria": "Sistemico", "texto": "¿Cómo entiende este marco teórico la psicoterapia de grupo? Nombrá Pros/Contras de éste modelo"},
     {"categoria": "Humanista", "texto": "¿Cuáles son los fundamentos de éste marco teórico? ¿Qué autores conoce?"},
